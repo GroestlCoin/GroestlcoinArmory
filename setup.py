849886f93f1e7c99d7bfad104e0d899f14daea26
ufasoft <dev@ufasoft.com>
2015-08-24 14:02:47 +0500

    -

--- setup.py
+++ setup.py
@@ -4,16 +4,17 @@ import py2exe
 
 
 opts = {"py2exe":{
-    "dll_excludes":["MSWSOCK.dll", "IPHLPAPI.dll", "MSWSOCK.dll", "WINNSI.dll", "WTSAPI32.dll"],
+    "dll_excludes":["MSWSOCK.dll", "IPHLPAPI.dll", "MSWSOCK.dll", "WINNSI.dll", "WTSAPI32.dll", "MSVCP90.dll"],
+	"includes":["sip"],
     "packages":["email"]
     }}
 
 setup( options = opts, windows = [
                         {
-                            "script": "../../ArmoryQt.py",
-                            "icon_resources": [(1, "../../img/armory256x256.ico"),
-                                               (1, "../../img/armory64x64.ico"),
-                                               (1, "../../img/armory48x48.ico")]
+                            "script": "../ArmoryQt.py",
+                            "icon_resources": [(1, "../img/armory256x256.ico"),
+                                               (1, "../img/armory64x64.ico"),
+                                               (1, "../img/armory48x48.ico")]
                         }
                 ]
     )
